18056218146913d032125689955a2339e875b6b5
Franklin <ffffranklin@pobox.com>
2015-09-13 12:40:13 -0700

    Applying full google styleguide

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var exec = require('child_process').exec;
 var format = require('util').format;
 var log = function log() {
-  return console.log.apply(this, arguments);
+  return console.log.apply(null, arguments);
 };
 var env = {
   host: process.env.DS_HOST,
@@ -49,17 +49,28 @@ var scotty = function scotty() {
 
   var repoManager = {
     cloneRepo: function cloneRepo(opts) {
-      var command = format('git clone --bare %s %s', opts.localRepoPath, opts.cloneRepoPath);
+      var srcPath = opts.localRepoPath;
+      var destPath = opts.cloneRepoPath;
+      var command = format('git clone --bare %s %s', srcPath, destPath);
 
-      return runCommand(command, 'Cloning repo to %', opts.cloneRepoPath);
+      return runCommand(command, 'Cloning repo to %', destPath);
     },
     copyRepoToServer: function copyRepoToServer(opts) {
-      var command = format('scp -r -P%d %s root@%s:%s', env.port, opts.cloneRepoPath, env.host, env.repoPath);
+      var cmd = 'scp -r -P%d %s root@%s:%s';
+      var port = env.port;
+      var src = opts.cloneRepoPath;
+      var host = env.host;
+      var repoPath = env.repoPath;
+      var command = format(cmd, port, src, host, repoPath);
 
       return runCommand(command, 'Pushing repo to server');
     },
     makeRemoteServerWriteable: function makeRemoteServerWriteable(opts) {
-      var command = format('ssh -p%d root@%s chmod -R g+rwX%s/%s', env.port, env.host, env.repoPath,  opts.cloneRepoPath);
+      var cmd = 'ssh -p%d root@%s chmod -R g+rwX %s';
+      var port = env.port;
+      var host = env.host;
+      var targetPath = format('%s/%s', env.repoPath, opts.cloneRepoPath);
+      var command = format(cmd, port, host, targetPath);
 
       return runCommand(command, 'Fixing permissions');
     },
@@ -72,7 +83,11 @@ var scotty = function scotty() {
 
   var methods =  {
     'list': function list() {
-      var command = format('ssh root@%s -p%d ls %s', env.host, env.port, env.repoPath);
+      var cmd = 'ssh root@%s -p%d ls %s';
+      var host = env.host;
+      var port = env.port;
+      var targetPath = env.repoPath;
+      var command = format(cmd, host, port, targetPath);
 
       exec(command, function(err, data) {
         log(data);
@@ -88,17 +103,21 @@ var scotty = function scotty() {
         hostRepoName: hostRepoName
       };
 
-      Promise.resolve()
-        .then(repoManager.cloneRepo(opts))
-        .then(repoManager.copyRepoToServer(opts))
-        .then(repoManager.makeRemoteServerWriteable(opts))
-        .then(repoManager.cleanUp(opts));
+      Promise.resolve().then(
+        repoManager.cloneRepo(opts)
+      ).then(
+        repoManager.copyRepoToServer(opts)
+      ).then(
+        repoManager.makeRemoteServerWriteable(opts)
+      ).then(
+        repoManager.cleanUp(opts)
+      );
     }
   };
 
   var run = function(method, args) {
     if (methods.hasOwnProperty(method)) {
-      methods[method].apply(this, args);
+      methods[method].apply(null, args);
     } else {
       log('scotty: FATAL_ERROR: Method "%s" does not exist', method);
     }
