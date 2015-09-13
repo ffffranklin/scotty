c96e39fb9b44a11358e88e3e641dd9e62bbbbcd7
Franklin <ffffranklin@pobox.com>
2015-09-13 11:32:32 -0700

    Adding eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@
 
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var eslint = require('gulp-eslint');
 
 gulp.task('test', shell.task([
   'tape test/**/*.js | faucet',
 ]));
 
-gulp.task('watch', ['test'], function() {
+gulp.task('lint', function () {
+  return gulp.src(['index.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('watch', ['test', 'lint'], function() {
   gulp.watch(['index.js', 'test/**/*.js'], ['test']);
+  gulp.watch(['index.js', 'test/**/*.js'], ['lint']);
 });
 
 gulp.task('default', ['test']);
