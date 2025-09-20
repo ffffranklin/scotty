02bb9f1a98019f1be7b077b4960e4ae0442b1835
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-09-20 16:52:28 -0700

    Upgrade meow to 9 and declare help menu with template ticks

--- bin/cli.js
+++ bin/cli.js
@@ -13,32 +13,32 @@ var pkg = require('../package.json');
 var meow = require('meow');
 
 var cli =  meow({
-  help: [
-    'Usage',
-    '  $ '+'scotty'.red.bold+' [COMMAND]'.green.bold+' [OPTIONS]'.magenta.bold,
-    '',
-    'Commands'.white.bold,
-    '  list'.white.bold,
-    '    Lists the repo folders',
-    '  add'.white.bold,
-    '    Clone your local repo to the server as a bare repo',
-    '  destroy'.white.bold,
-    '    Delete remote repo',
-    '',
-    'Options'.white.bold,
-    '  --'+'user'.green+'          SSH username',
-    '  --'+'host'.green+'          Git server hostname',
-    '  --'+'port'.green+'          Git server SSH port number [Default: 22]',
-    '  --'+'repoPath'.green+'      Path to repo parent directory on server',
-    '',
-    'Examples'.white.bold,
-    '  $ scotty list',
-    '  $ scotty add repo/path --user=git',
-    '  $ scotty destroy remote/path --user=git --host=10.0.0.25 --port=1022 --repoPath=/volume1/git',
-    '',
-    'Tips'.white.bold,
-    '  Put options in .bashrc instead of using flags for convenience'
-  ],
+  help: `
+    Usage
+      $ ${'scotty'.red.bold} ${'[COMMAND]'.green.bold} ${'[OPTIONS]'.magenta.bold}
+
+    ${'Commands'.white.bold}
+      ${'list'.white.bold}
+        Lists the repo folders
+      ${'add'.white.bold}
+        Clone your local repo to the server as a bare repo
+      ${'destroy'.white.bold}
+        Delete remote repo
+
+    ${'Options'.white.bold}
+      --${'user'.green}          SSH username
+      --${'host'.green}          Git server hostname
+      --${'port'.green}          Git server SSH port number [Default: 22]
+      --${'repoPath'.green}      Path to repo parent directory on server
+
+    ${`Examples`.white.bold}
+      $ scotty list
+      $ scotty add repo/path --user=git
+      $ scotty destroy remote/path --user=git --host=10.0.0.25 --port=1022 --repoPath=/volume1/git
+
+    ${'Tips'.white.bold}
+      Put options in .bashrc instead of using flags for convenience
+  `,
   pkg: pkg
 });
 
