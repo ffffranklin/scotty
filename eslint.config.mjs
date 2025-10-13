ec9a636d7f1e1bb43106e6669196ea5d968a6948
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-10-13 13:44:54 -0700

    Add typescript support for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
 import js from "@eslint/js";
+import ts from 'typescript-eslint';
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js, ts },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node }
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } }
 ]);
